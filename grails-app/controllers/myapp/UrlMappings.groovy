22951890265c46135300e12a583435eccd9275bc
h1ruma <hadi.nugraha93@gmail.com>
2018-09-05 16:24:41 +0800

    Tambah komentar agar lebih jelas

--- grails-app/controllers/myapp/UrlMappings.groovy
+++ grails-app/controllers/myapp/UrlMappings.groovy
@@ -9,7 +9,7 @@ class UrlMappings {
             }
         }
 
-        "/"(controller:"home")
+        "/"(controller:"home") //folder:"nama file"
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
